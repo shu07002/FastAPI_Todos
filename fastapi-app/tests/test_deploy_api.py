1fb55aab865cbe746ceff0a923c5689d383dae81
shu07002 <shu07002@gmail.com>
2025-10-06 16:24:19 +0900

    fix: add requests in requirements.txt and fix test deploy api

--- fastapi-app/tests/test_deploy_api.py
+++ fastapi-app/tests/test_deploy_api.py
@@ -1,6 +1,6 @@
 import requests
 
-BASE_URL = "http://3.35.93.179:8002"  # 배포된 서버 주소
+BASE_URL = "http://13.125.148.251:8002"  # 배포된 서버 주소
 
 def test_server_health():
     """서버가 응답하는지 확인"""
@@ -10,6 +10,7 @@ def test_server_health():
 def test_create_todo():
     """To-Do 생성이 잘 되는지 확인"""
     data = {
+        "id": 1,
         "title": "Jenkins Test",
         "description": "Created from Jenkins test stage",
         "completed": False
